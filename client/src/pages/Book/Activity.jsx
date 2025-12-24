042bb5c107a522f688b713ac68d156b76bad8bab
stonks <colinbexell@gmail.com>
2025-12-24 12:06:58 +0100

    progress

--- client/src/pages/Book/Activity.jsx
+++ client/src/pages/Book/Activity.jsx
@@ -1,22 +1,12 @@
 import React from "react";
 import "./Activity.css";
 
-const Activity = ({ title, img }) => {
+const Activity = ({ title, img, info }) => {
   return (
     <div className="main_card">
       <img src={img} alt="" />
-      <p>{title}</p>
-      <p className="info">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam
-        explicabo quisquam sapiente voluptatem accusamus repudiandae error ipsum
-        repellat quas reiciendis magni corporis officiis autem dicta qui, vel
-        harum hic ullam! Sapiente ut deserunt saepe minus placeat laudantium
-        repudiandae necessitatibus eos ratione fugiat. Blanditiis suscipit
-        excepturi quaerat porro culpa quod distinctio fugit libero quidem illum
-        aperiam voluptates iste inventore deleniti vitae, eveniet dignissimos
-        eum provident hic. Magni minima officiis eaque, nostrum eius similique
-        doloremque hic, nesciunt totam ipsa libero tempore maiores?
-      </p>
+      <h3>{title}</h3>
+      <p className="info">{info}</p>
     </div>
   );
 };
--- client/src/pages/Book/Book.jsx
+++ client/src/pages/Book/Book.jsx
@@ -3,7 +3,12 @@ import "./Book.css";
 import Lenis from "lenis";
 import Navbar from "../../components/navbar/Navbar";
 import Activity from "./Activity";
+
 import Bowling from "../../assets/Bowling.jpg";
+import Curling from "../../assets/Curling.jpg";
+import Dart from "../../assets/Dart.jpg";
+import Pingpong from "../../assets/Pingpong.png";
+import Pool from "../../assets/Pool.jpg";
 
 const Home = () => {
   const [page, setpage] = useState(1);
@@ -13,7 +18,46 @@ const Home = () => {
       case 1:
         return (
           <div className="content">
-            <Activity title={"Bowling"} img={Bowling} path={"test"} />
+            <Activity
+              title={"Bowling"}
+              img={Bowling}
+              path={"test"}
+              info={
+                "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially"
+              }
+            />
+            <Activity
+              title={"Curling"}
+              img={Curling}
+              path={"test"}
+              info={
+                "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially"
+              }
+            />
+            <Activity
+              title={"Dart"}
+              img={Dart}
+              path={"test"}
+              info={
+                "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially"
+              }
+            />
+            <Activity
+              title={"Pool"}
+              img={Pool}
+              path={"test"}
+              info={
+                "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially"
+              }
+            />
+            <Activity
+              title={"Pingpong"}
+              img={Pingpong}
+              path={"test"}
+              info={
+                "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially"
+              }
+            />
           </div>
         );
       case 2:
